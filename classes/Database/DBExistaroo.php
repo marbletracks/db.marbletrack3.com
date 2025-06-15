a341bcd6b8deec8a4e241da6f1208d340435bfe4
Rob Nugen <rob@robnugen.com>
2025-06-15 23:44:46 +0900

    You're on four on your directory; where can you go from there?

--- classes/Database/DBExistaroo.php
+++ classes/Database/DBExistaroo.php
@@ -159,11 +159,16 @@ class DBExistaroo {
         $applied = $this->getAppliedVersions();
         $base_dir = $this->config->app_path . "/db_schemas";
 
-        foreach (["00", "01", "02", "03", "04"] as $prefix) {
-            // Get all schema directories that match the prefix
-            $schema_dirs = glob("$base_dir/{$prefix}_*", GLOB_ONLYDIR);
+        // Find all schema directories matching NN_name, e.g. 00_core, 01_users
+        $schema_dirs = glob("$base_dir/[0-9][0-9]_*", GLOB_ONLYDIR);
+
+        // Sort to ensure order: 00_, 01_, ..., 10_
+        usort($schema_dirs, function ($a, $b) {
+            return strcmp(basename($a), basename($b));
+        });
 
             foreach ($schema_dirs as $schema_dir) {
+
                 $version = basename($schema_dir);   // directory name in $base_dir, e.g. "02_workers"
                 // print_rob($version, false);
                 $create_files = glob("$schema_dir/create_*.sql");
@@ -178,7 +183,6 @@ class DBExistaroo {
                     }
                 }
             }
-        }
 
         return $pending;
     }
