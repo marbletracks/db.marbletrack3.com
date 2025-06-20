d5673cca8e5826fdaec2f873e9c428898277be59
Rob Nugen <rob@robnugen.com>
2025-06-20 18:56:18 +0900

    Use Traits for thumbnailing images

--- templates/admin/parts/part.tpl.php
+++ templates/admin/parts/part.tpl.php
@@ -1,7 +1,3 @@
-<?php
-require_once __DIR__ . '/../../lib/thumbnail_for_string.php';
-?>
-
 <div class="PagePanel">
     <h1><?= $part ? 'Edit Part' : 'Create Part' ?></h1>
 
@@ -32,7 +28,7 @@ require_once __DIR__ . '/../../lib/thumbnail_for_string.php';
             Image URLs:<br>
             <div id="image-url-fields">
 <?php foreach ($image_urls ?? [''] as $url): ?>
-    <img src="<?= thumbnail_for_string(url: $url, maxWidth: 100) ?>" alt="Image preview" style="max-width: 100px; max-height: 100px;"><br>
+    <img src="<?= $part->thumbnailFor(url: $url, maxWidth: 100) ?>" alt="Image preview" style="max-width: 100px; max-height: 100px;"><br>
                 <input type="text" size=130 name="image_urls[]" value="<?= htmlspecialchars($url) ?>"><br>
 <?php endforeach; ?>
                 <!-- add empty row so we always have space -->
