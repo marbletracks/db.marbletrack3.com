4dd33d2681f386198c74f8233dd8f3172726708a
Rob Nugen <rob@robnugen.com>
2025-06-23 01:27:05 +0900

    Can create multiple photos (but only view one ugh)

--- db_schemas/03_photos/create_photos_table.sql
+++ db_schemas/03_photos/create_photos_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE photos (
     photo_id INT AUTO_INCREMENT PRIMARY KEY,
-    code CHAR(16) UNIQUE,      -- for CDN CloudFront etc
+    code CHAR(16),      -- for CDN CloudFront etc
     url VARCHAR(255)          -- fallback or legacy URL
 );
