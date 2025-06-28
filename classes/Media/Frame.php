345a8a05f8eb0bcf9c0a2fe6711813670fe0ca52
Rob Nugen <rob@robnugen.com>
2025-06-28 21:18:09 +0900

    Reframing Frames as not a good idea

--- classes/Media/Frame.php
+++ classes/Media/Frame.php
@@ -2,10 +2,16 @@
 namespace Media;
 
 /**
- * Frames are part of the final output movie or snippets.
- * They are taken during livestreams, and attached to Episodes
- * but are distinct from Photos.
+ * 21:15pm 28 June 2025
+ * Frames are too numerous to all be stored on DH.
+ *
+ * ~~21:05pm 28 June 2025~~
+ * ~~Frames are part of the final output movie or snippets.~~
+ * ~~They are taken during livestreams, and attached to Episodes~~
+ * ~~but are distinct from Photos.~~
  */
-class Frame extends Photo
+/*
+ class Frame extends Photo
 {
 }
+*/
