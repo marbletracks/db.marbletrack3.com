f15e20ca32e92167e831995b3b54ee9718e77217
Rob Nugen <rob@robnugen.com>
2025-06-30 23:04:39 +0900

    First draft load Parts Shortcodes

---
+++ wwwroot/admin/ajax/shortcode_filter.php
@@ -0,0 +1,25 @@
+<?php
+// /admin/ajax/shortcode_filter.php?q=ds
+declare(strict_types=1);
+include_once "/home/dh_fbrdk3/db.marbletrack3.com/prepend.php";
+
+if (!$is_logged_in->isLoggedIn()) {
+    header("HTTP/1.1 403 Forbidden");
+    exit;
+}
+
+$q = $_GET['q'] ?? '';
+$res = [];
+
+if ($q !== '') {
+    $q = trim($q);
+    $partsRepo = new \Database\PartsRepository($mla_database, "en");
+    $res = $partsRepo->searchByShortcodeOrName(
+        like: $q,
+        lang: "en",
+        limit: 10
+    );
+}
+
+header('Content-Type: application/json');
+echo json_encode($res);
