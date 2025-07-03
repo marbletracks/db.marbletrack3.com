d3dbe03bf907079324a673decf85d2b074edd7f9
Rob Nugen <thunderrabbit.2025_jun_30@lemur13.wasabi>
2025-07-03 16:09:54 +0900

    Create rudimentary worker detail template for frontend

---
+++ templates/workers/worker.tpl.php
@@ -0,0 +1,22 @@
+<div class="PagePanel">
+
+    Name:<br>
+    <?= htmlspecialchars($worker->name ?? '') ?>
+
+    Alias:<br>
+    <?= htmlspecialchars($worker->worker_alias ?? '') ?>
+
+    Description:<br>
+    <?= htmlspecialchars($worker->description ?? '') ?>
+    <br>
+    <div id="image-url-fields">
+        <?php if (!empty($worker->photos)):
+            foreach ($worker->photos ?? [''] as $photo): ?>
+                <a href="<?= $photo->getThumbnailUrl(); ?>">
+                    <img src="<?= $photo->getThumbnailUrl() ?>" alt="Image preview"><br>
+                </a>
+            <?php endforeach; ?>
+        <?php endif; ?>
+        <!-- add empty row so we always have space -->
+    </div>
+</div>
