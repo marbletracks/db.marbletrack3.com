b268585a7a0421d19505a18271ed20083e402ee6
Rob Nugen <thunderrabbit.2025_jun_30@lemur13.wasabi>
2025-07-06 20:32:58 +0900

    Show correct platform

--- templates/admin/poll/twitch_livestreams.tpl.php
+++ templates/admin/poll/twitch_livestreams.tpl.php
@@ -8,7 +8,7 @@
                 <li>
                     <?= htmlspecialchars($res['status']) ?>: <strong><?= htmlspecialchars($res['title']) ?></strong>
                     <?php if (!empty($res['url'])): ?>
-                        <a href="<?= htmlspecialchars($res['url']) ?>" target="_blank">Watch on Twitch</a><br>
+                        <a href="<?= htmlspecialchars($res['url']) ?>" target="_blank">Watch on <?= $platform ?></a><br>
                     <?php endif; ?>
                     <?php if (!empty($res['thumbnail_url'])): ?>
                         <img src="<?= htmlspecialchars($res['thumbnail_url']) ?>" alt="Thumbnail" style="max-height: 120px;"><br>
