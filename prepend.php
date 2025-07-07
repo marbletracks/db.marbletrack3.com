b94cd0ee23f7c5ca888388ce9695f7f2bd5f94c8
Rob Nugen <thunderrabbit.2025_jun_30@lemur13.wasabi>
2025-07-07 12:33:55 +0900

    BEGIN expand parts and workers shortcodes

--- prepend.php
+++ prepend.php
@@ -1,6 +1,6 @@
 <?php
 
-const SENTIMENTAL_VERSION = "Clean up parts in episodes";
+const SENTIMENTAL_VERSION = "Expand parts and workers in parts and workers";
 
 # write errors to screen
 ini_set('display_errors', 1);
