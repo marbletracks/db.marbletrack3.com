c2b99a5db85fcb99f2b69d08318f619ac2f82cde
Rob Nugen <thunderrabbit.2025_jun_30@lemur13.wasabi>
2025-07-09 15:32:53 +0900

    Moments will have translations.   Phrases are just shorthand

--- db_schemas/09_phrases_and_moments/create_phrases.sql
+++ db_schemas/09_phrases_and_moments/create_phrases.sql
@@ -1,7 +1,6 @@
 CREATE TABLE phrases (
     phrase_id INT AUTO_INCREMENT PRIMARY KEY,
     phrase VARCHAR(255) NOT NULL,
-    language_code CHAR(2) DEFAULT 'en',
     token_json VARCHAR(100), -- Optional: token breakdown
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
