0f8a1fa592d28c9b1eb7ea266ed273de2055e44d
Rob Nugen <thunderrabbit.2025_jun_30@lemur13.wasabi>
2025-07-09 15:25:09 +0900

    Tighten up Moments list a bit

--- templates/admin/moments/index.tpl.php
+++ templates/admin/moments/index.tpl.php
@@ -3,13 +3,16 @@
     <ul>
         <?php foreach ($moments as $moment): ?>
             <li>
-                <strong>ID <?= htmlspecialchars((string)$moment->moment_id) ?>:</strong>
+                <a href="/admin/moments/moment.php?id=<?= $moment->moment_id ?>">
+                    edit ID <?= htmlspecialchars((string)$moment->moment_id) ?>:
+                </a>
 <?php if ($moment->photos[0]): ?>
                     <img src="<?= htmlspecialchars($moment->photos[0]->getUrl()) ?>" alt="Moment photo" style="max-width: 100px; max-height: 100px;"><br>
 <?php endif; ?>
                 <?= htmlspecialchars($moment->notes ?? '(no notes)') ?><br>
+<?php if ($moment->phrase_id): ?>
                 <em>Phrase ID:</em> <?= htmlspecialchars((string)$moment->phrase_id ?? '—') ?><br>
-                <a href="/admin/moments/moment.php?id=<?= $moment->moment_id ?>">Edit</a>
+<?php endif; // ($moment->phrase_id): ?>
             </li>
         <?php endforeach; ?>
     </ul>
