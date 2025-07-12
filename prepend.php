b460bcf98358799de1d8cafa00e56b989f260acc
Rob Nugen <thunderrabbit.2025_jun_30@lemur13.wasabi>
2025-07-12 21:08:57 +0900

    BEGIN Create Significant Moments

--- prepend.php
+++ prepend.php
@@ -1,6 +1,6 @@
 <?php
 
-const SENTIMENTAL_VERSION = "Simplify Moment tables on backend";
+const SENTIMENTAL_VERSION = "Create Significant Moments";
 
 # write errors to screen
 ini_set('display_errors', 1);
